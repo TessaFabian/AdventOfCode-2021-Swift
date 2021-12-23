66e109fdcb78e3961a10faf8ea193c1a660100f4
TessaFabian <tessa.fabian@web.de>
2021-12-24 00:17:27 +0100

    Day 01 solved

--- aoc2021_day01.playground/Contents.swift
+++ aoc2021_day01.playground/Contents.swift
@@ -1,3 +1,34 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+let input = """
+199
+200
+208
+210
+200
+207
+240
+269
+260
+263
+"""
+
+
+var lines = input.components(separatedBy: "\n")
+var numbers = [Int]()
+for line in lines {
+    
+    numbers.append(Int(line) ?? 0)
+}
+let counterNumbers = numbers.count
+var counter = 0
+
+for i in 1...counterNumbers - 1 {
+    if numbers[i] > numbers[i-1] {
+        counter += 1
+    }
+}
+
+print(counter)
+
+
